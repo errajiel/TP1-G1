88fce78675bcc4bbe5147f0589ceb74218b71a47
errajiel <el.erraji@uca.ac.ma>
2023-10-12 14:09:56 +0100

    depart

---
+++ Exo3.java
@@ -0,0 +1,11 @@
+public class Exo3 {
+    public static void main(String args[]){
+        /**
+         * Écrire un programme en JAVA qui prend en entrée un nombre
+         *  entier et qui retourne le même nombre avec ses chiffres
+         *  en doublon supprimés.
+         * Entrée : java Exo3 122345
+         * Sortie : 12345
+         */
+    }
+}
---
+++ Exo4.java
@@ -0,0 +1,11 @@
+public class Exo4 {
+    public static void main(String args[]){
+        /**
+         * Ecrire un programme qui prend en argument un nombre réel
+         * et puis calcule la somme de ces chiffres en utilisant les
+         * methodes récursive.
+         * Input : java Exo4 5482
+         * Output : 5+4+8+2=19
+         */
+    }
+}
